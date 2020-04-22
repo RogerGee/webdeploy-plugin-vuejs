ec202e0fff15440fd9258f90a4c7c9cf21c32057
Roger Gee <roger.gee@tulsalibrary.org>
2020-04-22 17:07:53 -0500

    Add initial vuejs build functionality
    Adds functionality that parses a SFC and generates the script target.

---
+++ builder.js
@@ -0,0 +1,122 @@
+/**
+ * builder.js
+ *
+ * vuejs (webdeploy plugin)
+ *
+ * Copyright (C) Roger Gee, et al.
+ */
+
+const { format } = require("util");
+const vuejsCompiler = require('vue-template-compiler');
+const vuejsTranspile = require('vue-template-es2015-compiler');
+const hash = require('hash-sum');
+const postcss = require('postcss');
+
+function createTemplateFunction(code,isFunctional) {
+    var funcCode;
+
+    if (isFunctional) {
+        var opts = {
+            transforms: {
+                stripWithFunctional: true
+            }
+        };
+
+        funcCode = format("function render (_h, _vm) {%s}",code);
+        return vuejsTranspile(funcCode,opts);
+    }
+
+    funcCode = format("function render () {%s}",code);
+    return vuejsTranspile(funcCode);
+}
+
+function compileTemplate(template) {
+    const isFunctional = !!template.attrs.functional;
+
+    var compiled = vuejsCompiler.compile(template.content, {
+        preserveWhitespace: true
+    });
+
+    return {
+        render: createTemplateFunction(compiled.render),
+        staticRenderFns: '['
+            + compiled.staticRenderFns.map(createTemplateFunction).join(',')
+            + ']',
+        isFunctional
+    };
+}
+
+class VueBuilder {
+    constructor(target,settings) {
+        this.target = target;
+
+        // Create a scope ID for the builder. This will be used for scoped
+        // CSS. (This copies the behavior of vue-loader.)
+        this.scopeId = "data-v-" + hash(this.target.getSourceTargetPath());
+
+        this.compilerSettings = settings.vuejsCompilerSettings;
+    }
+
+    build(donefn) {
+        var targets = [];
+
+        // Parse the SFC target into its components.
+        var components;
+        components = vuejsCompiler.parseComponent(this.target.content,this.compilerSettings);
+
+        if (!components.script || !components.script.content) {
+            throw new Error(
+                format(
+                    "vuejs: target component '%s' must have a <script> section",
+                    this.target.targetName
+                )
+            );
+        }
+
+        // Compile the <template> section into render function object (if any).
+        var renderInfo;
+        if (components.template) {
+            renderInfo = compileTemplate(components.template);
+        }
+
+        // Create style targets from the <style> section(s).
+        var styleTargets = [];//TODO
+
+        var scriptContent = components.script.content;
+
+        // Create new JavaScript target that combines the render function (if
+        // any) and <script>.
+        var scriptTarget = this.createScriptTarget(scriptContent,renderInfo);
+
+        targets.push(scriptTarget);
+        styleTargets.forEach((target) => targets.push(target));
+
+        donefn(targets);
+    }
+
+    createScriptTarget(scriptContent,renderInfo) {
+        var scriptTarget = this.target.makeOutputTarget(this.target.targetName + ".js");
+        var content = format("// Compiled from %s\n",this.target.targetName) + scriptContent;
+
+        // Append render information to the script target's module exports.
+        if (renderInfo) {
+            content += "\n";
+            content += "export {\n";
+            content += "  render: " + renderInfo.render + ",\n";
+            content += "  staticRenderFns: " + renderInfo.staticRenderFns + ",\n";
+            content += "  functional: " + (renderInfo.isFunctional ? "true" : "false") + ",\n";
+            content += "  _compiled: true\n";
+            content += "};\n";
+        }
+
+        // Append scope ID to script target's module exports.
+        content += format("\nexport var _scopeId = '%s';",this.scopeId);
+
+        scriptTarget.stream.end(content);
+        return scriptTarget;
+    }
+}
+
+module.exports = {
+    VueBuilder
+}
